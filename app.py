d84ff36acf0af7505601032c105451a3df0d235d
Arfyan <r.arfyan@gmail.com>
2019-01-16 19:35:14 +0700

    first commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_html_components as html
 import numpy as np
 import plotly.plotly as py
 import plotly.graph_objs as go
-# import pandas as pd
+
 from plotly import tools
 from dash.dependencies import Input, Output
 from sqlalchemy import create_engine
